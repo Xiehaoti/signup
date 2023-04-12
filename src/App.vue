3361ad67136a2b02479a888bd7504d9009d9caa9
Xiehaoti <798860020@qq.com>
2023-04-12 09:53:21 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+    <el-container>
+      <el-header height="20px">网站图标</el-header>
+      <el-main>
+        <HelloWorld />
+      </el-main>
+      <el-footer height="20px">页脚</el-footer>
+  </el-container>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #1d1d1d;
+  margin-top: 60px;
+}
+.el-main{
+  margin-bottom: 30px;
+}
+body{
+  margin: 0;
+  padding: 0;
+  background-color: rgb(255, 255, 255);
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,45 @@
+<template>
+      <el-space direction="vertical">
+        <el-text class="mx-1" size="large">创建你的账号</el-text>
+        <el-text class="mx-1" size="small">创建你的账号</el-text>
+        <el-input v-model="input" placeholder="Email" />
+        <el-button color="#062036" :dark="isDark">注册</el-button>
+        <div class="dividerW">
+          <el-text class="mx-1" size="small">已经有账号了? <el-link type="primary">点击登录</el-link></el-text>
+          <el-divider>
+            or
+          </el-divider>
+        </div>
+        <el-button :icon="Search">Google</el-button>
+        <el-button :icon="Search">FaceBook</el-button>
+      </el-space>
+</template>
+
+<script setup>
+import { Search } from '@element-plus/icons-vue'
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+.el-space{
+  width: 300px;
+  height: 300px;
+}
+p{
+  font-size: 10px;
+}
+.el-button{
+  width: 192.5px;
+}
+body{
+
+}
+.el-link{
+font-size: 12px;
+}
+.dividerW{
+  width: 192.5px;
+}
+
+</style>
